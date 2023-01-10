fae273de910ee33cfdab4c3fef675bc67936d9d9
Smith Dabre <dabresmith97@gmail.com>
2023-01-09 23:38:39 -0500

    Initial commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by user221684 on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
